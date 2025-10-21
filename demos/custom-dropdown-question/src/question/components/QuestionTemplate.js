6f9177b24dfd3b9407401ec8f547c7001cb098a9
Alexandre Rivest <arivest@alloprof.qc.ca>
2025-10-21 11:57:37 -0400

    custom react dropdown

---
+++ demos/custom-dropdown-question/src/question/components/QuestionTemplate.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from "react";
+import {Dropdown} from "./Dropdown";
+
+export const TemplateRenderer = ({template, question, onDropdownChange, responseValue}) => {
+    const parts = template.split('{{dropdown}}');
+
+    return (
+        <div>
+            {parts.map((htmlPart, index) => (
+                <React.Fragment key={index}>
+                    {htmlPart && (
+                        <span dangerouslySetInnerHTML={{ __html: htmlPart }} />
+                    )}
+                    {index < parts.length - 1 && (
+                        <Dropdown
+                            value={responseValue[0]}
+                            options={question.dropdown_configs?.[index]?.options ?? []}
+                            placeholder={question.dropdown_configs?.[index]?.placeholder ?? ""}
+                            onChange={(value) => onDropdownChange(index, value)}
+                        />
+                    )}
+                </React.Fragment>
+            ))}
+        </div>
+    );
+};
